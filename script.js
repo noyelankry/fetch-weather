4a1914af143588e40d3487902f1e6b0a24003d06
noyelankry <noy.elankry@gmail.com>
2022-03-26 14:45:05 +0300

    weather API

--- script.js
+++ script.js
@@ -1,112 +1,118 @@
-
-let input = document.getElementById('artist-input')
+let input = document.getElementById('city-input')
 console.log(input)
+
 input.addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
-        // event.preventDefault()
-        // document.getElementById('search-btn').click()
-        searchArtist()
+        searchCity()
     }
 })
 
-function searchArtist() {
-    //delete the results for the previous search
-    document.getElementsByClassName('albums')[0].innerHTML = ''
+function createHtmlInfo(item) {
+    const template = `
+    <div class="location-details">
+        <h1>${item.location.name}</h1> 
+        (${item.location.region})
+        ${item.location.country} 
+        <br>
+        ${item.location.localtime} 
+        <br>
+        <br>
+    </div>
+    <h3>Currently:</h3>
+    <div class='current-weather'>
+        <br>
+            <img src="${item.current.condition.icon}" alt="weather-icon">
+            <br>
+            ${item.current.temp_c}&#8451;
+            <br>
+            ${item.current.condition.text}
+    </div>
+    <br>
+    <div id="forecast">
+        <h3>Forecast:</h3>
+        <br>
+    </div>
+    `
+    return $(template)
+}
 
-    //get artist name from the input value and transform it to lowercase
-    let artistName = document.getElementsByClassName('artist-input')[0].value
-    artistName = artistName.toLowerCase()
-    //if the artist name is empty the page will reload
-    if (artistName === '') {
+function renderChart(cData, fData, day, div) {
+    JSC.Chart(div, {
+        type: 'line',
+        title_label_text: `24-hour forecast for ${day}`,
+        series: [{
+            name: 'celsius',
+            points: cData
+        },
+        {
+            name: 'fahrenheit',
+            points: fData
+        }],
+        xAxis_scale_type: "hour",
+        legend_template: '%name %icon %average',
+        maintainAspectRatio: false
+    })
+}
+
+//API Key:
+const apiKey = '630053394cd9439ea0b53254222603'
+
+function searchCity() {
+    //delete the results for the previous search
+    document.getElementsByClassName('results')[0].innerHTML = ''
+
+    //get city name from the input value and transform it to lowercase
+    let cityName = document.getElementsByClassName('city-input')[0].value
+    cityName = cityName.toLowerCase()
+    //if the city name is empty the page will reload
+    if (cityName === '') {
         document.location.reload()
     }
+    let daysNum = document.getElementById('days-num').value
+    //if the user didn't select a number of days - alert and return.
+    if (daysNum === '0') {
+        alert('must choose number of days')
+        return
+    }
+
     document.getElementById('search-btn').setAttribute('disabled', true)
 
-    if (document.getElementsByName('API')[0].checked) {
-        //turn whitespaces to dashes
-        artistName = artistName.replace(/\s/g, '-')
-        console.log(artistName)
-        fetch(`https://itunes.apple.com/search?term=${artistName}`)
-            .then(response => {
-                response.json().then(data => {
-                    console.log('data', data)
+    cityName = cityName.replace(/\s/g, '-')
+    console.log(cityName)
 
-                    for (i = 0; i < data.results.length; i++) {
-                        let newLine = document.createElement('div')
-                        newLine.classList.add('album-name')
-                        newLine.innerHTML = `${i + 1}. 
-                        <strong>Artist Name</strong>: ${data.results[i].artistName} <br>
-                        <strong>Album Name</strong>: ${data.results[i].collectionName} <br>
-                        <strong>Listen Now (Preview)</strong>:
-                        <div>
-                            <audio src=${data.results[i].previewUrl} controls class='audio-player' /> 
-                        </div> <br>
-                        <img src='${data.results[i].artworkUrl100}' class = 'artwork-img'/> <br><br>`
-                        document.getElementsByClassName('albums')[0].append(newLine)
-                    }
-                    document.getElementById('search-btn').removeAttribute('disabled')
-                    if (document.getElementsByClassName('album-name').length != 0) {
-                        if (document.getElementById('panda') !== null) {
-                            document.getElementById('panda').remove()
-                            document.getElementById('listen-to').remove()
-                        }
-                    }
-                })
-            })
-    } else {
-        //turn whitespaces to %20
-        artistName = artistName.replace(/\s/g, '%20')
-        console.log(artistName)
-        const clientId = '1866bcd156c04721bd5d669b75a0d5f7'
-        const clientSecret = '3418b612e667483096772b57fb704ddb'
-
-        fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-            },
-            body: 'grant_type=client_credentials'
-        }).then(response => {
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=${daysNum}&aqi=no&alerts=no`)
+        .then(response => {
             response.json().then(data => {
-                token = (data.access_token)
-                console.log(token)
-                fetch(`https://api.spotify.com/v1/search?q=${artistName}&type=album`, {
-                    method: 'GET',
-                    headers: { 'Authorization': 'Bearer ' + token }
-                }).then(response1 => {
-                    response1.json().then(data1 => {
-                        console.log('data1', data1.albums.items)
-                        console.log('data2', (data1.albums.items.length))
+                console.log('data', data)
+                $('.results').append(createHtmlInfo(data))
 
-                        for (i = 0; i < data1.albums.items.length; i++) {
-                            let newLine = document.createElement('div')
-                            newLine.classList.add('album-name')
-                            newLine.innerHTML = `${i + 1}. 
-                            <strong>Artist Name</strong>: ${data1.albums.items[i].artists[0].name} <br>
-                            <strong>Album Name</strong>: ${data1.albums.items[i].name} <br>
-                            <strong> Listen on Spotify </strong>:
-                                <a href=${data1.albums.items[i].external_urls.spotify} class='click-here' target = "_blank"> 
-                                    Click here!
-                                <a/><br>
-                            <img src="${data1.albums.items[i].images[0].url}" class='artwork-img'/>
-                            <br><br>`
-                            document.getElementsByClassName('albums')[0].append(newLine)
-                        }
-                        document.getElementById('search-btn').removeAttribute('disabled')
-                        if (document.getElementsByClassName('album-name').length != 0) {
-                            if (document.getElementById('panda') !== null) {
-                                document.getElementById('panda').remove()
-                                document.getElementById('listen-to').remove()
-                            }
-                        }
-                    })
-                })
+                let item, cData, fData, div, divEl
 
+                for (let i = 0; i < data.forecast.forecastday.length; i++) {
+                    item = data.forecast.forecastday[i]
+                    cData = Object.keys(item.hour).map(x => ([Number(x), item.hour[x].temp_c]))
+                    fData = Object.keys(item.hour).map(x => ([Number(x), item.hour[x].temp_f]))
+
+                    div = `chartDiv${i}`
+                    divEl = document.createElement('div')
+                    divEl.setAttribute('id', div)
+                    console.log(divEl)
+                    $('#forecast').append(divEl)
+                    renderChart(cData, fData, item.date, div)
+                }
+
+                //after the results are done loading we can make the button functional again
+                document.getElementById('search-btn').removeAttribute('disabled')
+
+                if (document.getElementsByClassName('results').length != 0) {
+                    if (document.getElementById('astro') !== null) {
+                        document.getElementById('astro').remove()
+                        document.getElementById('going-to').remove()
+                    }
+                }
             })
         })
-    }
     //clear search input
-    document.getElementsByClassName('artist-input')[0].value = ''
+    document.getElementsByClassName('city-input')[0].value = ''
 }
 
